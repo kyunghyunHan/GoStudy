a6087daa702d096b3effa6da5ed466936a9ed8da
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-05-20 00:22:12 +0900

    문자열

---
+++ String/String.go
@@ -0,0 +1,13 @@
+package String
+
+import "fmt"
+
+func String(){
+
+	fmt.Println("문자열")
+}
+
+func Result(){
+
+	String()
+}
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import "github.com/kyunghyunHan/GoStudy/Var"
+import "github.com/kyunghyunHan/GoStudy/String"
 func main(){
 Var.Result()
+String.Result()
 
 }
