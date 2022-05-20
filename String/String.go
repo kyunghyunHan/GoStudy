0343f6e97de9dba20801ea212a39bda8d6d1fca1
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-05-20 21:58:22 +0900

    add

--- String/String.go
+++ String/String.go
@@ -3,8 +3,14 @@ package String
 import "fmt"
 
 func String(){
-
+	for i ,r := range "가나다" {
+		fmt.Println(i,r)
+	}
+	fmt.Println(len("가나다"))
 	fmt.Println("문자열")
+	for _,r := range "가가히" {
+		fmt.Println(string(r),r)
+	}
 }
 
 func Result(){
--- String/String.md
+++ String/String.md
@@ -1,1 +1,16 @@
 # 문자열
+- 바이트들이 연속적으로 나ㄹ
+
+## 유니코드 처리
+```go
+for i ,r := range "가나다" {
+    fmt.Println(i,r)
+}
+fmt.Println(len("가나다"))
+```
+- 가가히의 유니코드출력
+```go
+for _,r := range "가가히" {
+    fmt.Println(string(r),r)
+}
+```
