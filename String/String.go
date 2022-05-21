4a98fa446f35ccd3d84105825649737930778457
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-05-22 00:00:04 +0900

    add

--- String/String.go
+++ String/String.go
@@ -2,6 +2,21 @@ package String
 
 import "fmt"
 
+var(
+	start = rune(44032)//"가의 유니코드 포인트"
+	end = rune(55204)//"힣"다음 글자의 유니코드 포인트
+)
+func HasConsonanSuffix(s string) bool{
+	numEnds :=28
+	result := false
+	for _,r := range s{
+		if start <= r && r< end {
+			index:= int(r-start)
+			result = index%numEnds != 0
+		}
+	}
+	return result
+}
 func String(){
 	for i ,r := range "가나다" {
 		fmt.Println(i,r)
@@ -11,9 +26,17 @@ func String(){
 	for _,r := range "가가히" {
 		fmt.Println(string(r),r)
 	}
+	
 }
-
+func Example_printBytes(){
+	s:= "가나다"
+	for i := 0; i<len(s); i++{
+		fmt.Printf("%x:",s[i])
+	}
+	fmt.Println()
+}
 func Result(){
-
+	Example_printBytes()
 	String()
+	fmt.Println(HasConsonanSuffix("안녕"))
 }
--- String/String.md
+++ String/String.md
@@ -14,3 +14,18 @@ for _,r := range "가가히" {
     fmt.Println(string(r),r)
 }
 ```
+```go
+//한글이 받침이 있는지 없는지 확인
+func HasConsonanSuffix(s string) bool{
+	numEnds :=28
+	result := false
+	for _,r := range s{
+		if start <= r && r< end {
+			index:= int(r-start)
+			result = index%numEnds != 0
+		}
+	}
+	return result
+}
+```
+## 바이트단위 처리
