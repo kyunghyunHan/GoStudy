3ca41eb21f3612a4e93f07bac5729c012080f0cf
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-05-29 23:13:30 +0900

    add

--- Func/Func.go
+++ Func/Func.go
@@ -1,17 +1,32 @@
 package Func
-import "fmt"
-func AddOnd(nums []int){
-for i := range nums{
-	nums[i]++
+
+import (
+	"fmt"
+	"io"
+)
+
+func AddOnd(nums []int) {
+	for i := range nums {
+		nums[i]++
+	}
 }
-}
-func ExampleAddone(){
-	n:= []int{1,2,3,4}
+func ExampleAddone() {
+	n := []int{1, 2, 3, 4}
 	AddOnd(n)
 	fmt.Println(n)
-	
-}
 
-func Result(){
-   ExampleAddone()
+}
+func writeTo(w io.Writer, lines []string) (n int64, err error) {
+	for _, line := range lines {
+		var nw int
+		nw, err = fmt.Fprintln(w, line)
+		n += int64(nw)
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+func Result() {
+	ExampleAddone()
 }
